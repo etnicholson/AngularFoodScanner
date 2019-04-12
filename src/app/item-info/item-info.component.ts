04f9b02b3d218022657fc8cb5371998f33c7106e
etnicholson <45635838+etnicholson@users.noreply.github.com>
2019-04-12 19:35:56 -0400

    Update item-info.component.ts

--- src/app/item-info/item-info.component.ts
+++ src/app/item-info/item-info.component.ts
@@ -19,7 +19,7 @@ export class ItemInfoComponent implements OnInit {
     this.upc = this.route.snapshot.params.upc;
     this.http.get('https://api.edamam.com/api/food-database/parser?upc=' +
     this.upc +
-    '&app_id=0ae6af72&app_key=12eade76ad5a3dbc2b3c636647f81741')
+    '&app_id=YOUR_APP_ID&app_key=YOUR_APP_KEY')
     .subscribe((res: any ) => {
       this.maintext = res.hints[0].food;
       console.log(this.maintext);
